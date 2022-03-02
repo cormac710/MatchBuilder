8267b2791731c89668711faede8e5b8255e8cd0f
cormac <cormac_hallinan@hotmail.com>
2022-03-02 17:03:43 +0000

    cleaning

--- src/utilities/environment_variables.py
+++ src/utilities/environment_variables.py
@@ -1,11 +1,11 @@
 from os import environ
 
-# Dynamo
+# Dynamo vars
 REGION = environ.get('REGION', 'us-east-1')
 # https://dynamodb.{GPR_AWS_REGION}.amazonaws.com
 HOST = environ.get('HOST', 'http://127.0.0.1:8000')
 WRITE_CAPACITY = environ.get('WRITE_CAPACITY', 1)
 READ_CAPACITY = environ.get('READ_CAPACITY', 1)
 
-# flask app
+# flask app vars
 ROOT_PORT = environ.get('ROOT_PORT', 5000)
