63b8edb0d4e04c0d772c14c2d3c333df794a69e3
cormac <cormac_hallinan@hotmail.com>
2022-03-04 13:05:19 +0000

    player scan fix

--- src/dynamo_models/player.py
+++ src/dynamo_models/player.py
@@ -79,13 +79,13 @@ class Player(CommonModel):
         self.save()
 
     @classmethod
-    def query_by_range(cls, min_age=None, max_age=None, limit=100, lastkey=None):
+    def query_by_range(cls, min_age=None, max_age=None, limit=None, lastkey=None):
         condition = None
         if min_age:
             condition &= Player.age >= int(min_age)
         if max_age:
             condition &= Player.age <= int(max_age)
-        return Player.scan(filter_condition=condition, limit=int(limit), last_evaluated_key=lastkey)
+        return Player.scan(filter_condition=condition, limit=int(limit) if limit else 100, last_evaluated_key=lastkey)
 
     def as_json(self):
         player = super(Player, self).as_json()
